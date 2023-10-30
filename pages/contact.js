d62fe5a02508df537515eced8dc70966953e42e1
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-10-30 15:43:21 +0000

    email functionality added

--- pages/contact.js
+++ pages/contact.js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 // import { Inter } from 'next/font/google';
 import Heading from '@/components/Heading';
 import Paragraph from '@/components/Paragraph';
-
+import ContactForm from '@/components/forms/ContacForm';
 import { Button, DeleteIcon } from '@/components/mui/index';
+import { sendEmail } from '@/lib/api-functions/client';
 
 import Layout from '@/components/Layout';
 // const inter = Inter({ subsets: ['latin'] });
@@ -19,11 +20,16 @@ export default function Contact() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <Heading component='h2' variant="h3">Contact Page</Heading>
-        <Paragraph>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus quas quo voluptatum voluptatem vel reiciendis dolor necessitatibus nulla fuga quisquam ea, asperiores, earum eligendi? Nam dignissimos maxime voluptatem dolor nostrum.</Paragraph>
-        <Button variant='contained'>
-          <DeleteIcon />Delete
-        </Button>
+        <Heading component="h2" variant="h3">
+          Contact Us
+        </Heading>
+        <Paragraph>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          quas quo voluptatum voluptatem vel reiciendis dolor necessitatibus
+          nulla fuga quisquam ea, asperiores, earum eligendi? Nam dignissimos
+          maxime voluptatem dolor nostrum.
+        </Paragraph>
+        <ContactForm submitHandler={sendEmail}/>
       </Layout>
     </>
   );
